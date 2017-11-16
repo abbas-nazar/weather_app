0fae7a830203315c6e52331e6908ded1aa82ccaf
Abbas Nazar <abbasnazar.970@gmail.com>
2017-11-16 23:44:55 +0500

    weatherApiConnected&Working

--- app/src/main/java/com/abbasnazar/weatherapp/MainActivity.java
+++ app/src/main/java/com/abbasnazar/weatherapp/MainActivity.java
@@ -1,18 +1,26 @@
 package com.abbasnazar.weatherapp;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -29,13 +37,17 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar bar;
     ListView list;
     ArrayList<RowItem> rowitems;
+    String key="&appid=41401b36116d0ee4c2fdbef310679323";
     SharedPreferences sharedPreferences;
     String city,tempUnit;
     private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
     private static String IMG_URL = "http://openweathermap.org/img/w/";
     Weather w=new Weather();;
+
+    TextView cityname,temp,main,day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,21 +57,30 @@ public class MainActivity extends AppCompatActivity {
         rowitems=new ArrayList<RowItem>();
         list=(ListView)findViewById(R.id.weatherlist);
 
+        //bar=(ProgressBar)findViewById(R.id.progressBar);
+
         sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
         city=sharedPreferences.getString("city","");
+
+        temp=(TextView)findViewById(R.id.temp);
+        cityname=(TextView)findViewById(R.id.city);
+        main=(TextView)findViewById(R.id.condition);
+        day=(TextView)findViewById(R.id.current);
+
+
         /*if(city.equals(""))
         {
             Intent i=new Intent(this, setting.class);
             startActivity(i);
 
         }*/
-        TempCheck("lahore");
+
+        TempCheck("islamabad");
         ImageView im=(ImageView) findViewById(R.id.imageView);
         /*Bitmap bmp = BitmapFactory.decodeByteArray(w.icon, 0, w.icon.length);
         im.setImageBitmap(Bitmap.createScaledBitmap(bmp, im.getWidth(),
                 im.getHeight(), false));*/
-        TextView t=(TextView)findViewById(R.id.temp);
-        t.setText(w.average);
+
 
 
         fillArray();
@@ -82,8 +103,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void TempCheck(String c)
     {
-        Weathercheck weathercheck=new Weathercheck();
-        weathercheck.execute();
+
+        Weathercheck weathercheck=new Weathercheck(MainActivity.this);
+        weathercheck.execute(c);
+
+
     }
 
 
@@ -92,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         InputStream is = null;
 
         try {
-            con = (HttpURLConnection) ( new URL(BASE_URL + location)).openConnection();
+            con = (HttpURLConnection) ( new URL(BASE_URL + location+key)).openConnection();
             con.setRequestMethod("GET");
             con.setDoInput(true);
             con.setDoOutput(true);
@@ -156,19 +180,38 @@ public class MainActivity extends AppCompatActivity {
 
     private class Weathercheck extends AsyncTask<String, Integer, Weather> {
         String icon;
+        ProgressDialog progress;
+
+        public Weathercheck(Activity a)
+        {
+            progress=new ProgressDialog(a);
+        }
+        @Override
+        protected void onPreExecute() {
+
+            progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progress.setIndeterminate(true);
+            progress.show();
+        }
         protected Weather doInBackground(String... strng) {
 
             String data=getWeatherData(strng[0]);
             try {
                 JSONObject obj=new JSONObject(data);
                 w.name=obj.getString("name");
+                //Log.d("assassin",obj.getString("name"));
                 JSONArray arr=obj.getJSONArray("weather");
                 JSONObject o=arr.getJSONObject(0);
                 w.main=o.getString("main");
-                w.average=o.getString("temp");
-                w.min=o.getString("temp_min");
-                w.max=o.getString("temp_max");
+                //Log.d("assassin",o.toString());
+                JSONObject t=obj.getJSONObject("main");
+                Log.d("assassin",t.toString());
+                w.average=t.getString("temp");
+                w.min=t.getString("temp_min");
+                w.max=t.getString("temp_max");
                 icon=o.getString("icon");
+                Log.d("asfand",w.max);
+
             }
             catch (Exception e)
             {
@@ -182,9 +225,18 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        protected void onPostExecute(Long result) {
-
+        protected void onPostExecute(Weather result)
+        {
+            progress.dismiss();
+            update();
         }
+    }
+
+    public void update()
+    {
+        temp.setText(w.average);
+        main.setText(w.main);
+        cityname.setText(w.name);
     }
 
 }
